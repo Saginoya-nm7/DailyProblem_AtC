4ba03ca1e21891e3831f2eab237f1dc7baaf235d
Saginoya-nm7 <sqrt.trill425@gmail.com>
2020-07-07 01:38:39 +0900

    [Remove]不要な関数、不要なファイル

--- lib/Twitter/__init__.py
+++ lib/Twitter/__init__.py
@@ -8,13 +8,6 @@ class TwitterAPI:
     def __init__(self, client_key, client_secret, access_key, access_secret):
         self.client = OAuth1Session(client_key, client_secret, access_key, access_secret)
 
-    def getTimeline(self, count):
-        endpoint = "https://api.twitter.com/1.1/statuses/user_timeline.json"
-        param = {"count": count}
-        res = self.client.get(url=endpoint, params=param)
-
-        return responseParser(res)
-
     def tweet(self, tweet: str):
         endpoint = "https://api.twitter.com/1.1/statuses/update.json"
         param = {"status": tweet}
--- main.py
+++ main.py
@@ -6,6 +6,6 @@ problemDB = ProblemDB(config.USER_NAME)
 
 problem = problemDB.choose("d[400:800]")[0]
 print(problem)
-print(config.TWEET_TEMPLATE.format(
+print(config.TWEET_TEMPLATE[problem[6] == "Normal"].format(
     title=problem[1], contest_title=problem[3], point=int(problem[5]), url=urlTemplate.format(problem[2], problem[0]))
 )
